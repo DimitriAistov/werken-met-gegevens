c343b4fbcc92d1b8d605c95efe8bcc8af69f7e95
Dimitri <99058899@mydavinci.nl>
2021-10-26 11:27:24 +0200

    2

--- feestlunch.py
+++ feestlunch.py
@@ -1,6 +1,11 @@
 croissant, stokbrood = 0.39, 2.78
 kortingsbon = -0.50
 
+hoeveelheidCroissants = 17
+hoeveelheidStokbrood = 2
+hoeveelheidKortingsbonnen = 3
 
-print (croissant * 17 + stokbrood * 2 + kortingsbon * 3)
 
+prijs = croissant * hoeveelheidCroissants + stokbrood * hoeveelheidStokbrood + kortingsbon * hoeveelheidKortingsbonnen
+
+print('De feestlunch kost je bij de bakker: ' + str(prijs) + ' euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!')
--- speelhaldag.py
+++ speelhaldag.py
@@ -1,4 +1,8 @@
 ticket = 7.45
 vip = 0.37
+personen = 4
+tijd = 45/5
 
-prijs = print(ticket * 4 + vip * 9 * 4)
+prijs = ticket * personen + vip * tijd * personen
+
+print('Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar ' + str(prijs) + ' euro')
